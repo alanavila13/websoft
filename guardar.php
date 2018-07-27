b6a31a4712ddac1269361239c3483008e0a3ff99
alanavila13 <tacosricos98@hotmail.com>
2018-07-27 01:25:08 -0500

    almost done

--- guardar.php
+++ guardar.php
@@ -1,37 +1,40 @@
 <?php
-    //conectamos con el servidor
-    $conectar=@mysql_connect('localhost', 'root', '');
 
-    //verificar la conexion
-if(!$conectar){
-    echo"No se pudo conectar con el Servidor";
-}else{
-    $base=mysql_select_db('descripciones');
-    if(!$base){
-        echo"No se encontro la Base de Datos";
-    }
-}
-    //recuperar las variables
-    $nombre=$_POST['nombre'];
-    $apellido$_POST['apellido'];
-    $numero$_POST['numero'];
-    $correo$_POST['correo'];
-    $servicio$_POST['servicio'];
-    $descripcion$_POST['descripcion'];
-//Sentencia SQL
-$sql= "INSERT INTO descripciones VALUES('$nombre', 
+    $usuario = "root";
+    $password = "";
+    $servidor = "localhost";
+    $basededatos ="descripciones";
+
+
+
+$conexion = mysqli_connect  ($servidor,$usuario,"") or die ("No se ha podido conectar con el servidor de Base de datos // Couldn't connect with the database server");
+
+
+$db = mysqli_select_db($conexion, $basededatos) or die ("Upps! No se ha encontradpo la base de datos // Oops! Database not found");
+
+
+        //recuperar las variables
+        $nombre=$_POST['nombredesc'];
+        $apellido=$_POST['apellidodesc'];
+        $numero=$_POST['numerodesc'];
+        $correo=$_POST['correodesc'];
+        $servicio=$_POST['serviciodesc'];
+        $descripcion=$_POST['descripciondesc'];
+    //sentencia sql
+    $sql= "INSERT INTO descripciones VALUES('$nombre', 
                                         '$apellido',
-                                        '$numero',}
+                                        '$numero',
                                         '$correo',
                                         '$servicio',
                                         '$descripcion')";
-//ejecutamos la sentencia sql
-$ejecutar=mysql_query($sql);
-//verificamos la ejecucion
-if(!$ejecutar){
-    echo"Hubo algún error";
-}else{
-    echo"Datos Guardados Correctamente<a href='index.html'>Volver</a>";
-}
     
-?>
+    //ejecutamos la centencia de sql
+    $ejecutar=mysqli_query($conexion, $sql);
+    //verificacion de la ejecucioon
+    if(!$ejecutar){
+        echo"Hubo algun error";
+    }else{
+        echo"Tu idea fue enviada! Gracias por tu confianza // Your idea was sent successfully! Thanks for choosing us<br><a href='index.html'>Volver // Return</a>";
+    }
+     
+?>﻿
